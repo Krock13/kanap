0ed9875915db0955ae2dd4056a5f0c1b3f256735
krock13 <krock.airdrop@hotmail.com>
2022-10-06 18:41:53 +0200

    Revised script.js code for better readability

--- front/js/script.js
+++ front/js/script.js
@@ -12,35 +12,29 @@ function generateProductsCard(productList) {
         // Récupération de l'élément du DOM qui accueillera les fiches
         const sectionFiches = document.querySelector(".items");
 
-        // Creation d'un élément lien
+        // Création des éléments du DOM
         const linkElement = document.createElement("a");
-        // Recuperation de l'id du produit et ajout dans l'url pour utilisation par product.js
-        const id = productList[i]._id;
-        linkElement.href = "product.html" + "?_id=" + id;
-        // Lien relié à items
-        sectionFiches.appendChild(linkElement);
-
-        // Création d’une balise dédiée à un canapé
         const productElement = document.createElement("article");
-
-        // Création de l’élément img.
         const imageElement = document.createElement("img");
-        // Accès à l’indice i de la liste produit pour configurer la source de l’image
+        const nomElement = document.createElement("h3");
+        const descriptionElement = document.createElement("p");
+
+        // Affichage du nom et de la description du produit        
+        nomElement.innerText = productList[i].name;        
+        descriptionElement.innerText = productList[i].description;  
+
+        // Création de l'url qui servira à product.js
+        linkElement.href = "product.html" + "?_id=" + productList[i]._id;        
+        
+        // Définition des attributs de l'image du produit
         imageElement.src = productList[i].imageUrl;
         imageElement.setAttribute("alt", productList[i].altTxt);
-        // Rattachement de l’image à productElement (la balise article)
-        productElement.appendChild(imageElement);
 
-        // Idem pour le lien, le nom et la description
-        const nomElement = document.createElement("h3");
-        nomElement.innerText = productList[i].name;
-        productElement.appendChild(nomElement);
-
-        const descriptionElement = document.createElement("p");
-        descriptionElement.innerText = productList[i].description;
-        productElement.appendChild(descriptionElement);
-
-        // Rattachement de la balise article à la section fiches
+        // Attachement des éléments enfants
+        sectionFiches.appendChild(linkElement);
         linkElement.appendChild(productElement);
-    }
+        productElement.appendChild(imageElement);
+        productElement.appendChild(nomElement);
+        productElement.appendChild(descriptionElement);      
+    };
 };
